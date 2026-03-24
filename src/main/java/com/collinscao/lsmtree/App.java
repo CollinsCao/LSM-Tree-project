a64bdca9aec6118c16ef8a8fd23cd538a4ea1841
Collins Cao <YuqiCao99@gmail.com>
2026-03-24 14:23:28 -0700

    Initialize Maven project structure

---
+++ src/main/java/com/collinscao/lsmtree/App.java
@@ -0,0 +1,13 @@
+package com.collinscao.lsmtree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
